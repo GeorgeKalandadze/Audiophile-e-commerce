90a1ef33186c25a639989e312738fbaf81ee333f
GeorgeKalandadze <kalandadzeg777@gmail.com>
2023-04-02 14:07:14 +0400

    add form input wave on signup form

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import {useNavigate} from "react-router-dom"
 import { Button } from '../../components/Button/Button';
+import FormInputWave from '../../components/FormInputWave/FormInputWave';
 
 
 
@@ -18,10 +19,10 @@ const SignUp = () => {
     <div className='form'>
     <form >
         <h1>Sign Up</h1>
-        <input className='signinup-input'   name="name" placeholder={"Name"} />
-        <input className='signinup-input'  name="email"  placeholder={"Gmail"} />
-        <input className='signinup-input' name="password" placeholder={"Password"} />
-        <input className='signinup-input'  name="passwordConfirmation" placeholder={"Repeat password"} />
+        <FormInputWave id="name" label="Name" type="text" />
+        <FormInputWave id="email" label="Gmail" type="email" />
+        <FormInputWave id="email" label="Password" type="text" />
+        <FormInputWave id="email" label="Repeat password" type="text" />
         <Button width='100%' bgColor='#D87D4A' pdng='15px 25px'>Create An Account</Button>
         <p onClick={() => navigate(-1)}  className='form-type-text'>Already have an account? <span style={{color:"#D87D4A"}}>Login</span></p>
     </form>
